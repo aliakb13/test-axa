022bc6093822e8a2b9a980b6a012d25e4efad3b9
Ali <akbarsatriaxtkj2@gmail.com>
2025-06-12 19:24:23 +0700

    feat: update on update and delete users endpoint

--- src/main/java/com/example/axa_test/controller/UserController.java
+++ src/main/java/com/example/axa_test/controller/UserController.java
@@ -12,11 +12,15 @@ import com.example.axa_test.model.recordDto.UserResponse;
 import com.example.axa_test.service.UserService;
 
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PutMapping;
+
 
 
 @RestController
@@ -46,5 +50,18 @@ public class UserController {
       new BaseResponse<UserResponse>(HttpStatus.OK, "Berhasil mendapatkan data", userService.findUserById(id))
     );
   }
-  
+
+  @PutMapping("/{id}")
+  public ResponseEntity<?> updateUser(@PathVariable Long id, @RequestBody UserRequest request) {
+    return ResponseEntity.ok(
+      new BaseResponse<UserResponse>(HttpStatus.OK, "Berhasil memperbarui data!", userService.updateUser(id, request))
+    );
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<?> deleteUser(@PathVariable Long id) {
+    return ResponseEntity.ok(
+      new BaseResponse<String>(HttpStatus.OK, "Berhasil menghapus data!", "User dengan id: " + + userService.delete(id))
+    );
+  }
 }
--- src/main/java/com/example/axa_test/model/entities/Roles.java
+++ src/main/java/com/example/axa_test/model/entities/Roles.java
@@ -1,5 +1,33 @@
 package com.example.axa_test.model.entities;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class Roles {
-  
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  @Column(name = "role_id")
+  private Long roleId;
+
+  @Column(name = "role_name")
+  private String roleName;
+
+  @ManyToOne
+  @JoinColumn(name = "user_id")
+  private Users users;
 }
